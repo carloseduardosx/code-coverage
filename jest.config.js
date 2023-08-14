7a81b40ccb57f466ab80ae5c816b9e149db629a6
Carlos Eduardo <carloseduardosx2015@gmail.com>
2023-08-14 17:52:46 -0300

    Disable Jest thresholds

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,20 @@
 const config = {
   collectCoverage: true,
   collectCoverageFrom: ["./src/**/*.js"],
-  coverageThreshold: {
-    global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
-    },
-    './src/sum.js': {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    }
-  },
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 0,
+  //     functions: 0,
+  //     lines: 0,
+  //     statements: 0,
+  //   },
+  //   './src/sum.js': {
+  //     branches: 100,
+  //     functions: 100,
+  //     lines: 100,
+  //     statements: 100,
+  //   }
+  // },
   verbose: true,
 };
 

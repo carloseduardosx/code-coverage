ff4ea4a99b925a506531666d11bcd56d11717b0e
Carlos Eduardo <carloseduardosx2015@gmail.com>
2023-08-14 18:04:48 -0300

    Create test for base log

--- src/advanced-operations/__tests__/log.spec.js
+++ src/advanced-operations/__tests__/log.spec.js
@@ -4,4 +4,8 @@ describe('Log', () => {
   it('should calculate the logarithm of 10', () => {
     expect(log(10)).toBe(2.302585092994046);
   });
+
+  it('should calculate the base 2 logarithm of 10', () => {
+    expect(log(10, 2)).toBe(2.302585092994046);
+  });
 });
